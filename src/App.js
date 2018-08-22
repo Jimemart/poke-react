7f36e1be56002977e0eacb23672e189237da80b3
JimenaMartin <jimena.martin@cloudappi.net>
2018-08-22 22:26:38 +0200

    he

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 class App extends Component {
+    state = {
+      hello: "hello",
+    };
+
     render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to
+      const { hello } = this.state
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">
+Welcome to React
+              {hello}
+            </h1>
+          </header>
+          <p className="App-intro">
+                    To get started, edit
+            {' '}
+            <code>src/App.js</code>
+            {' '}
+and save to
                     reload.
-                </p>
-            </div>
-        );
+          </p>
+        </div>
+      )
     }
 }
 
-export default App;
+export default App
