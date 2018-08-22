1bb490d17e4ea72f0342bcdfd05aa2f41d28987a
JimenaMartin <jimena.martin@cloudappi.net>
2018-08-22 21:24:53 +0200

    works?

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it("ugly", () => {
+    expect(4).toBe(4);
+});
+
+it("failing", () => {
+    expect(4).toBe(5);
 });
