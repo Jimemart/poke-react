c6d33c73da27209185c1dfee95b9c3ff135df9f7
JimenaMartin <jimena.martin@cloudappi.net>
2018-08-24 09:26:46 +0200

    testcafe

--- e2e/fixtures/Pokemon.fixture.js
+++ e2e/fixtures/Pokemon.fixture.js
@@ -1,24 +1,18 @@
-import Page from '../pages/Pokemon.page'
-import { waitForReact } from 'testcafe-react-selectors'
+import Page from "../pages/Pokemon.page"
 
 const page = new Page()
 
-fixture(`Pokemon`)
-  .page(`http://127.0.0.1:3000`)
-  .beforeEach(async t => {
-    await waitForReact()
-  })
+fixture(`Pokemon`).page(`http://127.0.0.1:3000`)
 
-test('it should fetch a new pokemon on input', async t => {
-  await t.expect(page.firstTitle.exists).ok({timeout: 5000})
+test("it should fetch a new pokemon on input", async t => {
+  await t.expect(page.firstTitle.exists).ok({ timeout: 5000 })
   await t.expect(page.images.count).eql(4)
   await t.expect(page.bulbasaurHeight.exists).ok()
   await t.expect(page.bulbasaurWeight.exists).ok()
   await t.expect(page.natures.count).eql(2)
   await t.typeText(page.input, page.newPokemonId)
-  await t.expect(page.secondTitle.exists).ok({timeout: 5000})
+  await t.expect(page.secondTitle.exists).ok({ timeout: 5000 })
   await t.expect(page.charmanderHeight.exists).ok()
   await t.expect(page.charmanderWeight.exists).ok()
   await t.expect(page.natures.count).eql(1)
-
 })
